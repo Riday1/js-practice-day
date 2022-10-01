c126580944c67643fbf4a367fa330a3f554f3364
Riday1 <rrriday100@gmail.com>
2022-10-01 23:38:18 +0600

    problem-59

---
+++ problem-59.js
@@ -0,0 +1,26 @@
+
+
+const num1 = 20;
+const num2 = 10;
+
+if (num1 > num2) {
+    console.log(num1, 'is greater than', num2);
+}
+
+if (num2 < num1) {
+    console.log(num2, 'is lessthan ', num1);
+}
+
+if (num1 === num2) {
+    console.log(num1, 'and ', num2, 'is equal')
+}
+
+if (num1 !== num2) {
+    console.log(num1, 'and', num2, 'is not equal')
+}
+if (num1 >= num2) {
+    console.log(num1, 'is greaterthan or equal to ', num2)
+}
+if (num2 <= num1) {
+    console.log(num2, 'is lessthan or equal to', num1)
+}
