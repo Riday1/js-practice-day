e06a22bd18199df60bf1b2621eb86bd85aeb31d8
Riday1 <rrriday100@gmail.com>
2022-10-01 23:44:07 +0600

    problem- 60

---
+++ problem-60.js
@@ -0,0 +1,17 @@
+const raining = true;
+const taka = false;
+
+/* if (raining === true && taka === true) {
+    console.log('ajke vhuna khichuri + kalavhuna hobe , kop hobe vai shudhu kop')
+}
+else {
+    console.log('shob vhule jao , coding koro coding')
+} */
+
+if (raining === true || taka === true) {
+    console.log('ajke dal vhat khabo')
+}
+else {
+    console.log('ajke roja rakhbo')
+}
+
