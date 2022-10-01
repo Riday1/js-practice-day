3039fff423ab7d8bf49a938672899428ed34e89a
Riday1 <rrriday100@gmail.com>
2022-10-02 00:48:41 +0600

    problem-66

---
+++ problem-65.js
@@ -0,0 +1,6 @@
+const numbers = [20, 90, 40, 100, 81, 80, 99];
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 80) {
+        console.log(numbers[i])
+    }
+}
---
+++ problem-66.js
@@ -0,0 +1,6 @@
+function multiplicationOfThreeNumber(num1, num2, num3) {
+    return num1 * num2 * num3;
+}
+
+const result = multiplicationOfThreeNumber(2, 3, 4);
+console.log(result)
