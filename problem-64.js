a2487c3d7cfd4e0fe98e21c0b7e583fdcc924e53
Riday1 <rrriday100@gmail.com>
2022-10-02 00:38:27 +0600

    problem-64.js

---
+++ problem-64.js
@@ -0,0 +1,13 @@
+const friends = ['sabit', 'shorif', 'siddik', 'shishir'];
+
+//dispaly all array element by using for loop
+for (var i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+}
+
+
+// by using for of 
+
+for (const friend of friends) {
+    console.log(friend)
+}
